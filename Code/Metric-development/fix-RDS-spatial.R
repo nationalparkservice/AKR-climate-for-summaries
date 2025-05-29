98d13509043cd78ffe58ecde9194c5550f4d7f0c
Runyon <arunyon@nps.gov>
2025-05-29 14:37:13 -0600

    script to fix rds files

---
+++ Code/Metric-development/fix-RDS-spatial.R
@@ -0,0 +1,122 @@
+# Fixes deltas.RDS files so they compare future to Daymet instead of to historical modeled
+library(stars);library(dplyr);library(ggplot2);library(ggthemes); library(readxl); library(viridis);library(here);library(ggrepel);library(rlang);library(units)
+
+rm(list=ls())
+root.dir <- "C:/Users/arunyon/OneDrive - DOI/AKR-CFs/"
+
+projections <- read_excel(paste0("C:/Users/arunyon/OneDrive - DOI/AKR-CFs/", "AKCFs-Projections-used.xlsx"))
+
+historical.period <- as.character(seq(1950,1999,1))
+future.period <- as.character(seq(2035,2065,1))
+daymet.period <- as.character(seq(1980,2016,1))
+
+CFs <- c("Climate Future 1", "Climate Future 2")
+cols <- c("#6EB2D4","#CA0020")
+
+###
+##
+# This is where loop gets starts for parks
+SiteID = "ANIA" #Update or loop
+park <- SiteID
+data.dir <- paste0(root.dir,SiteID,"/Data/") 
+
+# Set projections
+GCMs <- unlist(projections[which(projections$SiteID == park),][2:3])
+CF_GCM <- data.frame(CF=CFs,GCM=GCMs,CF_col=cols)
+
+# Read in shapefile ??
+
+
+#### Tmean
+var = "Annual.tmeanF"
+
+## Read in and create mean obj for Daymet
+cropped_st_grid <- readRDS(paste(data.dir,"cropped_st_Daymet",sep="/"))
+grid_var <- list()
+
+for(F in 1:length(cropped_st_grid)){
+  s = cropped_st_grid[[F]]
+  s %>% mutate(tmean = (tmax + tmin)/2) -> s 
+  s = dplyr::select(s, tmean)
+  grid_var[[F]] = s[,,,] #all months
+}
+
+grid_var_stars <- Reduce(c, grid_var)
+grid_var_stars$tmean <- drop_units(grid_var_stars$tmean)
+grid_var_stars %>% mutate(tmean_f = tmean * 9/5 + 32) %>% dplyr::select(tmean_f) -> grid_var_stars
+
+by_t = "1 year"
+grid <- aggregate(grid_var_stars, by = by_t, FUN = mean, na.omit = TRUE) # Doesn't work in lat/long. Must be projected. Removes units from tmax. Also aggregates to a lower resolution.
+grid <- grid[,1:length(daymet.period),,]
+
+mean_grid <- st_apply(grid, c("x", "y"), mean)
+saveRDS(mean_grid, file = paste(data.dir,paste(var,"Daymet",sep="_"),sep="/"))
+
+
+for (G in 1:length(GCMs)){
+  gcm = sub("\\..*", "", GCMs[G])
+  rcp = sub('.*\\.', '', GCMs[G])
+  
+  cropped_st_fut <- readRDS(paste(data.dir,paste0("cropped_st_fut_",gcm,"_",rcp),sep="/"))
+  
+  fut_var <- list()
+  for(F in 1:length(cropped_st_fut)){
+    s = cropped_st_fut[[F]]
+    s %>% mutate(tmean = (tmax + tmin)/2) -> s 
+    s = dplyr::select(s, tmean)
+    fut_var[[F]] = s[,,,] #all months
+  }
+  
+  fut_var_stars <- Reduce(c, fut_var) 
+  fut_var_stars %>% mutate(tmean_f = tmean * 9/5 + 32) %>% dplyr::select(tmean_f) -> fut_var_stars
+
+  fut <- aggregate(fut_var_stars, by = by_t, FUN = mean, na.omit = TRUE) # Doesn't work in lat/long. Must be projected. Removes units from tmax. Also aggregates to a lower resolution.
+  fut <- fut[,1:length(future.period),,]
+
+  mean_fut <- st_apply(fut, c("x", "y"), mean)
+  mean_fut2 <- mean_fut %>% mutate(grid = as.vector(mean_grid$mean)) %>% dplyr::select(grid) #This is some fuckery that has to be done bececause Daymet grid is flipped somehow, puts it on same grid
+  flip <- st_flip(mean_fut2, "y") #flip Daymet
+  
+  delta <- mean_fut - flip
+  saveRDS(delta, file = paste(data.dir,paste(var,gcm,rcp,sep="_"),sep="/"))
+}
+
+
+
+
+
+######### EXTRA CODE USED TO FIGURE OUT FLIPPING - SAVE FOR NOW
+# Dimension columns are reversed
+st_get_dimension_values(mean_fut, 'y', max=TRUE) 
+
+fut_y <- st_get_dimension_values(mean_fut, 'y', max=TRUE) 
+fy <- fut_y[, rev(seq_len(ncol(fut_y)))] # This correctly reverses the dimension columns
+fy1 <- lapply(seq_len(ncol(fy)), function(i) fy[,i])
+
+mean_fut2 <- st_set_dimensions(mean_fut, 2, values = fy1 )
+
+
+
+as.vector(mean_grid$mean)
+
+
+mean_fut2 <- mean_fut %>% mutate(grid = as.vector(mean_grid$mean),
+                                 delta = mean - grid) %>% dplyr::select(grid)
+
+flip <- st_flip(mean_fut2, "y")
+
+delta <- mean_fut - flip
+
+
+
+ggplot() + # Resolution is course
+  geom_stars(data = delta, alpha = 0.8) + 
+  scale_y_reverse() +
+  # facet_wrap("time") +
+  # scale_fill_viridis() + 
+  #coord_equal() + 
+  theme_map() +
+  theme(legend.position = "bottom") +
+  theme(legend.key.width = unit(2, "cm"))
+
+
